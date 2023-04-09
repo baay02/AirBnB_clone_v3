ffd998e433e527f9c76da09d05f3890c19e6f566
baay02 <baaybilya25@yahoo.co>
2023-04-09 02:23:49 -0700

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,35 +1,52 @@
 #!/usr/bin/python3
-"""a script to send an archive file to a remote server
-and decompress it"""
+"""
+Fabric script that distributes an archive to your web servers
+"""
 
-from fabric.api import run, env, put
-import os.path
+from datetime import datetime
+from fabric.api import *
+import os
 
-env.hosts = ['54.164.55.231', '54.157.188.140']
-env.key_filename = '~/.ssh/school'
-env.user = 'ubuntu'
+env.hosts = ["54.164.55.231", "54.157.188.140"]
+env.user = "ubuntu"
+
+
+def do_pack():
+    """
+        return the archive path if archive has generated correctly.
+    """
+
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archived_f_path = "versions/web_static_{}.tgz".format(date)
+    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
+
+    if t_gzip_archive.succeeded:
+        return archived_f_path
+    else:
+        return None
 
 
 def do_deploy(archive_path):
-    """a function to deploy code and decompress it"""
-
-    if not os.path.isfile(archive_path):
-        return False
-    compressed_file = archive_path.split("/")[-1]
-    no_extension = compressed_file.split(".")[0]
-
-    try:
-        remote_path = "/data/web_static/releases/{}/".format(no_extension)
-        sym_link = "/data/web_static/current"
+    """
+        Distribute archive.
+    """
+    if os.path.exists(archive_path):
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
         put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(remote_path))
-        run("sudo tar -xvzf /tmp/{} -C {}".format(compressed_file,
-                                                  remote_path))
-        run("sudo rm /tmp/{}".format(compressed_file))
-        run("sudo mv {}/web_static/* {}".format(remote_path, remote_path))
-        run("sudo rm -rf {}/web_static".format(remote_path))
+        run("sudo mkdir -p {}".format(newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
+        run("sudo rm {}".format(archived_file))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        run("sudo rm -rf {}/web_static".format(newest_version))
         run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -sf {} {}".format(remote_path, sym_link))
+        run("sudo ln -s {} /data/web_static/current".format(newest_version))
+
+        print("New version deployed!")
         return True
-    except Exception as e:
-        return False
+
+    return False
