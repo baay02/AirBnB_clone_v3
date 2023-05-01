0e7c9d104ac0071ceb30d985ad319bb7d80f2c10
baay02 <baaybilya25@yahoo.com>
2023-05-01 19:17:26 +0000

    view/places

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,68 +1,64 @@
 #!/usr/bin/python3
-
-""" Handles all restful API actions for State"""
-
+""" place view """
 from api.v1.views import app_views
-from flask import request, jsonify, abort
+from flask import jsonify, Blueprint, make_response, abort, request
 from models import storage
+from models.place import Place
 from models.city import City
 from models.user import User
-from models.place import Place
+from models.base_model import BaseModel
 
 
-@app_views.route('cities/<city_id>/places',
-                 methods=['GET', 'POST'], strict_slashes=False)
-def places_by_city(city_id):
-    """retrieve places based on city_id"""
-
-    if request.method == 'GET':
-        city = storage.get(City, city_id)
-
-        if city:
-            places = [place.to_dict() for place in city.places]
-            return jsonify(places)
+@app_views.route('/cities/<city_id>/places', methods=["GET", "POST"],
+                 strict_slashes=False)
+def get_city_place(city_id):
+    """ gets all place objs of a city """
+    output = []
+    city = storage.get(City, city_id)
+    if city is None:
         abort(404)
-    elif request.method == 'POST':
-        city = storage.get(City, city_id)
-
-        if city:
-            my_dict = request.get_json()
-            if my_dict is None:
-                abort(400, 'Not a JSON')
-            if my_dict.get("user_id") is None:
-                abort(400, 'Missing user_id')
-            if my_dict.get("name") is None:
-                abort(400, 'Missing name')
-
-            user = storage.get(User, my_dict.get("user_id"))
-
-            if user:
-                place = Place(**my_dict)
-                place.save()
-                return jsonify(place.to_dict()), 201
+    if request.method == "GET":
+        for place in city.places:
+            output.append(place.to_dict())
+        return (jsonify(output))
+    if request.method == "POST":
+        data = request.get_json()
+        if not request.is_json:
+            abort(400, description="Not a JSON")
+        if 'user_id' not in request.json:
+            abort(400, description="Missing user_id")
+        user_id = data['user_id']
+        user = storage.get(User, user_id)
+        if user is None:
             abort(404)
-        abort(404)
+        if 'name' not in request.json:
+            abort(400, description="Missing name")
+        data['city_id'] = city_id
+        place = Place(**data)
+        place.save()
+        return (jsonify(place.to_dict()), 201)
 
 
-@app_views.route('/places/<string:place_id>',
-                 methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
-def place_by_place_id(place_id):
-    """Retrieves a place based on the place_id"""
+@app_views.route('/places/<place_id>', methods=[
+                 "GET", "PUT", "DELETE"], strict_slashes=False)
+def get_a_place(place_id):
+    """ retrieves one unique place object """
     place = storage.get(Place, place_id)
-
     if place is None:
         abort(404)
-    if request.method == 'GET':
-        return jsonify(place.to_dict())
-    if request.method == 'DELETE':
-        place.delete()
-        storage.save()
-        return jsonify({}), 200
-    if request.method == 'PUT':
-        my_dict = request.get_json()
-        if my_dict is None:
-            abort(400, 'Not a JSON')
-        for k, v in my_dict.items():
-            setattr(place, k, v)
+    if request.method == "GET":
+        output = place.to_dict()
+        return (jsonify(output))
+    if request.method == "PUT":
+        data = request.get_json()
+        if not request.is_json:
+            abort(400, description="Not a JSON")
+        for key, value in data.items():
+            setattr(place, key, value)
         place.save()
-        return jsonify(place.to_dict()), 200
+        return (jsonify(place.to_dict()), 200)
+    if request.method == "DELETE":
+        storage.delete(place)
+        storage.save()
+        result = make_response(jsonify({}), 200)
+        return result
